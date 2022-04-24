4647a4c6615ec9f641782da67fe82085c3433faf
Leonardo Passarini <leogpassarini@gmail.com>
2022-04-24 14:19:02 -0300

    Scroll Suave

--- script.js
+++ script.js
@@ -35,3 +35,20 @@ function initAccordion() {
   }
 }
 initAccordion();
+
+function initScrollSuave() {
+  const linkInterno = document.querySelectorAll('.menu a[href^="#"]');
+
+  linkInterno.forEach((item) => {
+    item.addEventListener("click", (event) => {
+      event.preventDefault();
+      const href = event.currentTarget.getAttribute("href");
+      const section = document.querySelector(href);
+      section.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    });
+  });
+}
+initScrollSuave();
