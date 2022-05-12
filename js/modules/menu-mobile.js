7f973c78e1d9153cd6a5bf01507ba203aefb9925
Leonardo Passarini <leogpassarini@gmail.com>
2022-05-12 17:57:32 -0300

    correção bug touchstart

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -3,7 +3,7 @@ import outsideClick from "./outsideclick.js";
 export default function initMenuMobile() {
   const menuButton = document.querySelector('[data-menu="button"]');
   const menuList = document.querySelector('[data-menu="list"]');
-  const eventos = ["click", "touchstart"];
+  const eventos = ["click", "touch"];
 
   if (menuButton) {
     function openMenu(event) {
